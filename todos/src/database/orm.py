6afe5fa5df650164c9112ac65937f3ca1cf10db5
nahyun <rsg0415@naver.com>
2026-09-06 23:24:49 +0900

    orm 실습 - 데이터베이스 테이블 조회

---
+++ todos/src/database/orm.py
@@ -0,0 +1,21 @@
+# orm은 데이터베이스 테이블과 매핑되는 클래스를 정의하는 모듈
+# 실습: orm을 사용하여 데이터베이스 테이블을 조회
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+### declarative_base()를 통해 Base 클래스 생성
+Base = declarative_base()
+
+class ToDo(Base):
+    __tablename__ = "todo"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    contents = Column(String(256), nullable=False)
+    is_done = Column(Boolean, nullable=False)
+
+    ### __repr__() 메서드 오버라이딩. ToDo 객체 출력 확인용
+    def __repr__(self): 
+        return f"ToDo(id={self.id}, contents={self.contents}, is_done={self.is_done})"
+    
+    
